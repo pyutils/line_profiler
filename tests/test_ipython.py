56e5a74d29421eb920f68a0547f2a4d474582801
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2022-02-23 23:28:19 +0100

    Add basic ipython test

---
+++ tests/test_ipython.py
@@ -0,0 +1,12 @@
+import unittest
+import io
+
+from IPython.testing.globalipapp import get_ipython
+
+class TestIPython(unittest.TestCase):
+    def test_init(self):
+        ip = get_ipython()
+        ip.magic('load_ext line_profiler')
+        ip.run_cell(raw_cell="def func():\n    return 2**20")
+        ip.run_line_magic('lprun', '-f func func()')
+        # TODO: Check output
