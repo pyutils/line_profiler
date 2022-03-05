583cf272b04e581c7975db1d0fa6ad52e356adc0
Joseph Martinot-Lagarde <contrebasse@gmail.com>
2022-03-06 00:09:31 +0100

    Fix deprecation warning

--- tests/test_ipython.py
+++ tests/test_ipython.py
@@ -6,7 +6,7 @@ from IPython.testing.globalipapp import get_ipython
 class TestIPython(unittest.TestCase):
     def test_init(self):
         ip = get_ipython()
-        ip.magic('load_ext line_profiler')
-        ip.run_cell(raw_cell="def func():\n    return 2**20")
+        ip.run_line_magic('load_ext', 'line_profiler')
+        ip.run_cell(raw_cell='def func():\n    return 2**20')
         ip.run_line_magic('lprun', '-f func func()')
         # TODO: Check output
