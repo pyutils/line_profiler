12ed5322bf118ad02a608a3a8a1143582a22e64a
joncrall <erotemic@gmail.com>
2023-08-03 14:37:26 -0400

    Fix win32

--- tests/test_complex_case.py
+++ tests/test_complex_case.py
@@ -41,6 +41,7 @@ def test_complex_example():
     import sys
     import pathlib
     import subprocess
+    import os
 
     try:
         test_dpath = pathlib.Path(__file__).parent
@@ -51,7 +52,7 @@ def test_complex_example():
     complex_fpath = test_dpath / 'complex_example.py'
 
     from subprocess import PIPE
-    proc = subprocess.run([sys.executable, complex_fpath], stdout=PIPE,
+    proc = subprocess.run([sys.executable, os.fspath(complex_fpath)], stdout=PIPE,
                           stderr=PIPE, universal_newlines=True)
     print(proc.stdout)
     print(proc.stderr)
