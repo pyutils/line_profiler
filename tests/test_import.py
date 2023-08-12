2ea46111f46eaabe57711b2db29470786f7f612c
joncrall <erotemic@gmail.com>
2023-08-12 16:02:18 -0400

    Better version test

--- tests/test_import.py
+++ tests/test_import.py
@@ -7,4 +7,9 @@ def test_import():
 def test_version():
     import line_profiler
     from packaging.version import Version
-    assert Version(line_profiler.__version__)
+    import kernprof
+    line_profiler_version1 = Version(line_profiler.__version__)
+    line_profiler_version2 = Version(line_profiler.line_profiler.__version__)
+    kernprof_version = Version(kernprof.__version__)
+    assert line_profiler_version1 == line_profiler_version2 == kernprof_version, (
+        'All 3 places should have the same version')
