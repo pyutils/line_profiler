257afa717f9c4caac21494528d30627179ad49ff
tai <tai@gameface.ai>
2023-08-14 02:16:39 +1000

    refactor autoprofile to use clearer function and variable names add docstrings and comments

--- line_profiler/autoprofile/ast_tree_profiler.py
+++ line_profiler/autoprofile/ast_tree_profiler.py
@@ -1,33 +1,87 @@
-import os
 import ast
+import os
 
-from line_profiler.autoprofile.ast_profle_transformer import AstProfileTransformer, ast_profile_module
-from line_profiler.autoprofile.profmod_extractor import ProfmodExtractor
+from .ast_profle_transformer import (AstProfileTransformer,
+                                     ast_create_profile_node)
+from .profmod_extractor import ProfmodExtractor
 
 
 class AstTreeProfiler:
+    """Create an abstract syntax tree of a script and add profiling to it.
+
+    Reads a script file and generates an abstract syntax tree, then adds nodes to the AST
+    that adds the specified functions/methods, classes & modules in prof_mod to the
+    profiler to be profiled.
+    """
+
     def __init__(self,
                  script_file,
                  prof_mod,
                  profile_imports,
                  ast_transformer_class_handler=AstProfileTransformer,
                  profmod_extractor_class_handler=ProfmodExtractor):
-        self.script_file = script_file
-        self.prof_mod = prof_mod
-        self.profile_imports = profile_imports
-        self.ast_transformer_class_handler = ast_transformer_class_handler
-        self.profmod_extractor_class_handler = profmod_extractor_class_handler
+        """Initializes the AST tree profiler instance with the script file path
+
+        Args:
+            script_file (str):
+                path to script being profiled.
+
+            prof_mod (List[str]):
+                list of imports to profile in script.
+                passing the path to script will profile the whole script.
+                the objects can be specified using its dotted path or full path (if applicable).
+
+            profile_imports (bool):
+                if True, when auto-profiling whole script, profile all imports aswell.
+
+            ast_transformer_class_handler (AstProfileTransformer):
+                the AstProfileTransformer class that handles profiling the whole script.
+
+            profmod_extractor_class_handler (ProfmodExtractor):
+                the ProfmodExtractor class that handles mapping prof_mod to objects in the script.
+        """
+        self._script_file = script_file
+        self._prof_mod = prof_mod
+        self._profile_imports = profile_imports
+        self._ast_transformer_class_handler = ast_transformer_class_handler
+        self._profmod_extractor_class_handler = profmod_extractor_class_handler
 
     @staticmethod
-    def _check_prof_mod_profile_full_script(script_file, prof_mod):
+    def _check_profile_full_script(script_file, prof_mod):
+        """Check whether whole script should be profiled.
+
+        Checks whether path to script has been passed to prof_mod indicating that
+        the whole script should be profiled
+
+        Args:
+            script_file (str):
+                path to script being profiled.
+
+            prof_mod (List[str]):
+                list of imports to profile in script.
+                passing the path to script will profile the whole script.
+                the objects can be specified using its dotted path or full path (if applicable).
+
+        Returns:
+            profile_full_script (bool):
+                if True, profile whole script.
+        """
         script_file_realpath = os.path.realpath(script_file)
-        for mod in prof_mod:
-            if os.path.realpath(mod) == script_file_realpath:
-                return True
-        return False
+        profile_full_script = script_file_realpath in map(os.path.realpath, prof_mod)
+        return profile_full_script
 
     @staticmethod
-    def _get_ast_tree(script_file):
+    def _get_script_ast_tree(script_file):
+        """Generate an abstract syntax from a script file.
+
+        Args:
+            script_file (str):
+                path to script being profiled.
+
+        Returns:
+            tree (_ast.Module):
+                abstract syntax tree of the script.
+        """
         with open(script_file, 'r') as f:
             script_text = f.read()
         tree = ast.parse(script_text, filename=script_file)
@@ -35,31 +89,74 @@ class AstTreeProfiler:
 
     def _profile_ast_tree(self,
                           tree,
-                          tree_names_to_profile_dict,
-                          profile_script=False,
-                          profile_script_imports=False):
+                          tree_imports_to_profile_dict,
+                          profile_full_script=False,
+                          profile_imports=False):
+        """Add profiling to an abstract syntax tree.
+
+        Adds nodes in the AST that adds the specified objects to the profiler.
+        If profile_full_script is True, all functions/methods, classes & modules in the script
+        have a node added to the AST to add them to the profiler.
+        If profile_imports is True as well as profile_full_script, all imports are have a node
+        added to the AST to add them to the profiler.
+
+        Args:
+            tree (_ast.Module):
+                abstract syntax tree to be profiled.
+
+            tree_imports_to_profile_dict (Dict[int,str]):
+                dict of imports to profile
+                    key (int):
+                        index of import in AST
+                    value (str):
+                        alias (or name if no alias used) of import
+
+            profile_full_script (bool):
+                if True, profile whole script.
+
+            profile_imports (bool):
+                if True, and profile_full_script is True, profile all imports aswell.
+
+        Returns:
+            tree (_ast.Module):
+                abstract syntax tree with profiling.
+        """
         profiled_imports = []
-        argsort_tree_indexes = sorted(list(tree_names_to_profile_dict), reverse=True)
+        argsort_tree_indexes = sorted(list(tree_imports_to_profile_dict), reverse=True)
         for tree_index in argsort_tree_indexes:
-            name = tree_names_to_profile_dict[tree_index]
-            expr = ast_profile_module(name)
+            name = tree_imports_to_profile_dict[tree_index]
+            expr = ast_create_profile_node(name)
             tree.body.insert(tree_index + 1, expr)
             profiled_imports.append(name)
-        if profile_script:
-            tree = self.ast_transformer_class_handler(profile_imports=profile_script_imports,
-                                                      profiled_imports=profiled_imports).visit(tree)
+        if profile_full_script:
+            tree = self._ast_transformer_class_handler(profile_imports=profile_imports,
+                                                       profiled_imports=profiled_imports).visit(tree)
         ast.fix_missing_locations(tree)
         return tree
 
     def profile(self):
-        profile_script = self._check_prof_mod_profile_full_script(self.script_file, self.prof_mod)
+        """Create an abstract syntax tree of a script and add profiling to it.
 
-        tree = self._get_ast_tree(self.script_file)
+        Reads a script file and generates an abstract syntax tree.
+        Then matches imports in the script's AST with the names in prof_mod.
+        The matched imports are added to the profiler for profiling.
+        If path to script is found in prof_mod, all functions/methods, classes & modules are
+        added to the profiler.
+        If profile_imports is True as well as path to script in prof_mod, all the imports
+        in the script are added to the profiler.
 
-        tree_names_to_profile_dict = self.profmod_extractor_class_handler(
-            tree, self.script_file, self.prof_mod).run()
+        Returns:
+            tree (_ast.Module):
+                abstract syntax tree with profiling.
+        """
+        profile_full_script = self._check_profile_full_script(self._script_file, self._prof_mod)
+
+        tree = self._get_script_ast_tree(self._script_file)
+
+        tree_imports_to_profile_dict = self._profmod_extractor_class_handler(
+            tree, self._script_file, self._prof_mod).run()
         tree_profiled = self._profile_ast_tree(tree,
-                                               tree_names_to_profile_dict,
-                                               profile_script=profile_script,
-                                               profile_script_imports=self.profile_imports)
+                                               tree_imports_to_profile_dict,
+                                               profile_full_script=profile_full_script,
+                                               profile_imports=self._profile_imports)
         return tree_profiled
--- line_profiler/autoprofile/autoprofile.py
+++ line_profiler/autoprofile/autoprofile.py
@@ -1,30 +1,48 @@
 import types
 
-from line_profiler.autoprofile.ast_tree_profiler import AstTreeProfiler
-from line_profiler.autoprofile.line_profiler_utils import add_imported_function_or_module
+from .ast_tree_profiler import AstTreeProfiler
+from .line_profiler_utils import add_imported_function_or_module
+
+PROFILER_LOCALS_NAME = 'prof'
 
 
-def extend_line_profiler_import_profiling(prof):
+def _extend_line_profiler_for_profiling_imports(prof):
+    """Allow @profile decorator to handle functions/methods, classes & modules.
+
+    Add a method to LineProfiler that can identify whether the object is a
+    function/method, class or module and handle it's profiling accordingly.
+    Mainly used for profiling objects that are imported.
+    (Workaround to keep changes needed by autoprofile separate from base LineProfiler)
+
+    Args:
+        prof (LineProfiler):
+            instance of LineProfiler.
+    """
     prof.add_imported_function_or_module = types.MethodType(add_imported_function_or_module, prof)
 
 
-def run(script_file, ns, prof_mod=None, profile_imports=False):
-    if prof_mod is None:
-        """
-        what should default behaviour be if no modules to profile?
-        full script profiling or no profiling?
-        code copied from kernfprof
-        """
+def run(script_file, ns, prof_mod, profile_imports=False):
+    """Automatically profile a script and run it.
 
-        def execfile(filename, globals=None, locals=None):
-            """ Python 3.x doesn't have 'execfile' builtin """
-            with open(filename, 'rb') as f:
-                exec(compile(f.read(), filename, 'exec'), globals, locals)
+    Profile functions, classes & modules specified in prof_mod without needing to add
+    @profile decorators.
 
-        execfile(script_file, ns, ns)
-        return
+    Args:
+        script_file (str):
+            path to script being profiled.
 
+        ns (dict):
+            "locals" from kernprof scope.
+
+        prof_mod (List[str]):
+            list of imports to profile in script.
+            passing the path to script will profile the whole script.
+            the objects can be specified using its dotted path or full path (if applicable).
+
+        profile_imports (bool):
+            if True, when auto-profiling whole script, profile all imports aswell.
+    """
     tree_profiled = AstTreeProfiler(script_file, prof_mod, profile_imports).profile()
 
-    extend_line_profiler_import_profiling(ns['prof'])
+    _extend_line_profiler_for_profiling_imports(ns[PROFILER_LOCALS_NAME])
     exec(compile(tree_profiled, script_file, 'exec'), ns, ns)
