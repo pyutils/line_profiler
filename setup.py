167003a0201e6baf9462732c09a41344877176eb
Marcin <marcin.konow@lczyk.xyz>
2025-01-29 14:11:44 +0000

    updated StaticVisitor

--- setup.py
+++ setup.py
@@ -55,10 +55,12 @@ def static_parse(varname, fpath):
     pt = ast.parse(sourcecode)
 
     class StaticVisitor(ast.NodeVisitor):
-        def visit_Assign(self, node):
+        def visit_Assign(self, node: ast.Assign):
             for target in node.targets:
                 if getattr(target, "id", None) == varname:
-                    self.static_value = node.value.s
+                    value: ast.expr = node.value
+                    if isinstance(value, ast.Constant) and isinstance(value.value, str):
+                        self.static_value = value.value
 
     visitor = StaticVisitor()
     visitor.visit(pt)
