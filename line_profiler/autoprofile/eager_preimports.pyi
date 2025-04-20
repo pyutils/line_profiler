f4e7e50b52d3afcd98dd175188c6bb82a6663e23
Terence S.-C. Tsang <t.tsang.sci@gmail.com>
2025-04-20 14:17:30 +0200

    WIP: eager on-import profiling
    line_profiler/autoprofile/eager_preimport.py[i]
        New module for generating a dummy module where the profiling targets
        are pre-imported, so that they can be added to the profiler;
        main functionalities:
        - `split_dotted_path()`: split a dotted path into a module part and
          an attribute part
        - `write_eager_import_module()`: write the text of a module which
          does all the supplied imports and adds them to the profiler

---
+++ line_profiler/autoprofile/eager_preimports.pyi
@@ -0,0 +1,48 @@
+import ast
+from typing import (
+    Any, Collection, Generator, List, Set, TextIO, Tuple, Union)
+
+
+def is_dotted_path(obj: Any) -> bool:
+    ...
+
+
+def get_expression(obj: Any) -> Union[ast.Expression, None]:
+    ...
+
+
+def split_dotted_path(dotted_path: str) -> Tuple[str, Union[str, None]]:
+    ...
+
+
+def strip(s: str) -> str:
+    ...
+
+
+class LoadedNameFinder(ast.NodeVisitor):
+    names: Set[str]
+    contexts: List[Set[str]]
+
+    def visit_Name(self, node: ast.Name) -> None:
+        ...
+
+    def visit_FunctionDef(self,
+                          node: Union[ast.FunctionDef, ast.AsyncFunctionDef,
+                                      ast.Lambda]) -> None:
+        ...
+
+    visit_AsyncFunctionDef = visit_Lambda = visit_FunctionDef
+
+    @classmethod
+    def find(cls, node: ast.AST) -> Set[str]:
+        ...
+
+
+def propose_names(prefixes: Collection[str]) -> Generator[str, None, None]:
+    ...
+
+
+def write_eager_import_module(
+        dotted_paths: Collection[str], stream: Union[TextIO, None] = None, *,
+        adder: str = 'profile.add_imported_function_or_module') -> None:
+    ...
