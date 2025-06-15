caa3b201da1d93168dcbd8af0da8b990438b5d4b
Terence S.-C. Tsang <t.tsang.sci@gmail.com>
2025-06-15 10:07:30 +0200

    Updated tests for eager pre-imports
    tests/test_eager_preimports.py
        sample_package()
            - Renamed from `sample_module()`
            - Simplified implementation
        preserve_sys_state(), sample_namespace_package()
            New fixtures
        gen_names()
            New utility function
        test_{split_dotted_path,resolve_profiling_targets}_staticity()
            New tests for how the `static` parameter influences the behavior
            of
            `line_profiler.autoprofile.eager_preimports.split_dotted_path()`
            and `.resolve_profiling_targets()`

--- tests/test_eager_preimports.py
+++ tests/test_eager_preimports.py
@@ -8,6 +8,7 @@ Most of the features are already covered by the doctests.
 import subprocess
 import sys
 from contextlib import ExitStack
+from functools import partial
 from pathlib import Path
 from operator import methodcaller
 from runpy import run_path
@@ -27,7 +28,7 @@ else:
     HAS_FLAKE8 = True
 
 from line_profiler.autoprofile.eager_preimports import (
-    write_eager_import_module as write_module)
+    split_dotted_path, resolve_profiling_targets, write_eager_import_module)
 
 
 def write(path: Path, content: Optional[str] = None) -> None:
@@ -38,19 +39,32 @@ def write(path: Path, content: Optional[str] = None) -> None:
         path.write_text(dedent(content).strip('\n'))
 
 
-@pytest.fixture
-def sample_module(tmp_path: Path) -> str:
-    """
-    Write a module and put it in :py:data:`sys.path`.  When we're done,
-    reset :py:data:`sys.path` and `sys.modules`.
-    """
-    def gen_names() -> Generator[str, None, None]:
-        while True:
-            yield 'my_module_' + str(uuid4()).replace('-', '_')
+def gen_names(name) -> Generator[str, None, None]:
+    while True:
+        yield '_'.join([name, *str(uuid4()).split('-')])
 
+
+@pytest.fixture
+def preserve_sys_state() -> None:
     old_path = sys.path.copy()
     old_modules = sys.modules.copy()
-    module_name = next(name for name in gen_names() if name not in old_modules)
+    try:
+        yield
+    finally:
+        sys.path.clear()
+        sys.path[:] = old_path
+        sys.modules.clear()
+        sys.modules.update(old_modules)
+
+
+@pytest.fixture
+def sample_package(preserve_sys_state: None, tmp_path: Path) -> str:
+    """
+    Write a normal package and put it in :py:data:`sys.path`.  When
+    we're done, reset :py:data:`sys.path` and `sys.modules`.
+    """
+    module_name = next(name for name in gen_names('my_sample_pkg')
+                       if name not in sys.modules)
     new_path = tmp_path / '_modules'
     write(new_path / module_name / '__init__.py')
     write(new_path / module_name / 'foo' / '__init__.py')
@@ -63,16 +77,28 @@ def sample_module(tmp_path: Path) -> str:
           raise AssertionError
           """)
     write(new_path / module_name / 'foobar.py')
-    try:
+    # Cleanup managed with `preserve_sys_state()`
+    sys.path.insert(0, str(new_path))
+    yield module_name
+
+
+@pytest.fixture
+def sample_namespace_package(
+        preserve_sys_state: None,
+        tmp_path_factory: pytest.TempPathFactory) -> str:
+    """
+    Write a namespace package and put it in :py:data:`sys.path`.  When
+    we're done, reset :py:data:`sys.path` and `sys.modules`.
+    """
+    module_name = next(name for name in gen_names('my_sample_namespace_pkg')
+                       if name not in sys.modules)
+    new_paths = [tmp_path_factory.mktemp('_modules-', numbered=True)
+                 for _ in range(3)]
+    for submod, new_path in zip(['one', 'two', 'three'], new_paths):
+        write(new_path / module_name / (submod + '.py'))
+        # Cleanup managed with `preserve_sys_state()`
         sys.path.insert(0, str(new_path))
-        yield module_name
-    finally:
-        sys.path.clear()
-        sys.path[:] = old_path
-        assert str(new_path) not in sys.path
-        sys.modules.clear()
-        sys.modules.update(old_modules)
-        assert module_name not in sys.modules
+    yield module_name
 
 
 @pytest.mark.parametrize(
@@ -85,11 +111,11 @@ def test_write_eager_import_module_wrong_adder(
     :py:meth:`~.write_eager_import_module()`.
     """
     with pytest.raises(xc):
-        write_module(['foo'], adder=adder)
+        write_eager_import_module(['foo'], adder=adder)
 
 
 @pytest.mark.skipif(not HAS_FLAKE8, reason='no `flake8`')
-def test_written_module_pep8_compliance(sample_module: str):
+def test_written_module_pep8_compliance(sample_package: str):
     """
     Test that the module written by
     :py:meth:`~.write_eager_import_module()` passes linting by
@@ -98,8 +124,9 @@ def test_written_module_pep8_compliance(sample_module: str):
     with TemporaryDirectory() as tmpdir:
         module = Path(tmpdir) / 'module.py'
         with module.open(mode='w') as fobj:
-            write_module([sample_module + '.foobar'],
-                         recurse=[sample_module + '.foo'], stream=fobj)
+            write_eager_import_module(
+                [sample_package + '.foobar'],
+                recurse=[sample_package + '.foo'], stream=fobj)
         print(module.read_text())
         (subprocess
          .run([sys.executable, '-m', 'flake8',
@@ -133,7 +160,7 @@ def test_written_module_pep8_compliance(sample_module: str):
      # error
      (['__MODULE__', '__MODULE__.foo.baz'], False, [], AssertionError)])
 def test_written_module_error_handling(
-        sample_module: str,
+        sample_package: str,
         dotted_paths: Collection[str],
         recurse: Union[Collection[str], bool],
         warnings: Sequence[Collection[str]],
@@ -143,7 +170,7 @@ def test_written_module_error_handling(
     :py:meth:`~.write_eager_import_module()` gracefully handles errors
     for implicitly included modules.
     """
-    replace = methodcaller('replace', '__MODULE__', sample_module)
+    replace = methodcaller('replace', '__MODULE__', sample_package)
     dotted_paths = [replace(target) for target in dotted_paths]
     if recurse not in (True, False):
         recurse = [replace(target) for target in recurse]
@@ -157,7 +184,8 @@ def test_written_module_error_handling(
             # warnings at module-generation time and execution time
             captured_warnings = enter(catch_warnings(record=True))
             with module.open(mode='w') as fobj:
-                write_module(dotted_paths, recurse=recurse, stream=fobj)
+                write_eager_import_module(
+                    dotted_paths, recurse=recurse, stream=fobj)
             print(module.read_text())
             if error is not None:
                 enter(pytest.raises(error))
@@ -169,3 +197,34 @@ def test_written_module_error_handling(
     for warning, fragments in zip(captured_warnings, warnings):
         for fragment in fragments:
             assert fragment in str(warning.message)
+
+
+def test_split_dotted_path_staticity() -> None:
+    """
+    Test `split_dotted_path()` with different values for `static`.
+    """
+    split = partial(split_dotted_path, 'os.path.abspath')
+    # Static analysis has no idea of `os.path` members since `os.path`
+    # is dynamically imported from e.g. `posixpath` or `ntpath`
+    assert split(static=True) == ('os', 'path.abspath')
+    # The import system knows of the already-imported module `os.path`
+    assert split(static=False) == ('os.path', 'abspath')
+
+
+def test_resolve_profiling_targets_staticity(
+        sample_namespace_package: str) -> None:
+    """
+    Test subpackage/-module discovery with `resolve_profiling_targets()`
+    with different values for `static`.
+    """
+    all_targets = ({f'{sample_namespace_package}.{submod}'
+                    for submod in ['one', 'two', 'three']}
+                   | {sample_namespace_package})
+    # Static analysis can't handle namespace packages
+    resolve = partial(
+        resolve_profiling_targets, [sample_namespace_package], recurse=True)
+    static_result = resolve(static=True)
+    assert set(static_result.targets) < all_targets, static_result
+    # The import system successfully retrieves all submodules
+    dyn_result = resolve(static=False)
+    assert set(dyn_result.targets) == all_targets, dyn_result
