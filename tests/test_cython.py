757add6da1e6f4b1b4240319cd1bd591961c371d
Terence S.-C. Tsang <t.tsang.sci@gmail.com>
2025-06-19 05:00:56 +0200

    Fixed (?) fixture teardown

--- tests/test_cython.py
+++ tests/test_cython.py
@@ -10,7 +10,6 @@ from importlib.util import find_spec
 from io import StringIO
 from operator import methodcaller
 from pathlib import Path
-from tempfile import TemporaryDirectory
 from types import ModuleType
 from typing import Generator, Tuple
 from uuid import uuid4
@@ -28,6 +27,7 @@ def propose_name(prefix: str) -> Generator[str, None, None]:
 
 
 def _install_cython_example(
+        tmp_path_factory: pytest.TempPathFactory,
         editable: bool) -> Generator[Tuple[Path, str], None, None]:
     """
     Install the example Cython module in a name-clash-free manner.
@@ -38,48 +38,47 @@ def _install_cython_example(
                   if not find_spec(name))
     replace = methodcaller('replace', 'cython_example', module)
     pip = [sys.executable, '-m', 'pip']
-    with TemporaryDirectory() as tmpdir:
-        # Replace all references to `cython_example` with the actual
-        # module name and write to the tempdir
-        tmp_path = Path(tmpdir)
-        for prefix, _, files in os.walk(source):
-            dir_in = Path(prefix)
-            for fname in files:
-                _, ext = os.path.splitext(fname)
-                if ext not in {'.py', '.pyx', '.pxd', '.toml'}:
-                    continue
-                dir_out = tmp_path.joinpath(*(
-                    replace(part)
-                    for part in dir_in.relative_to(source).parts))
-                dir_out.mkdir(exist_ok=True)
-                file_in = dir_in / fname
-                file_out = dir_out / replace(fname)
-                file_out.write_text(replace(file_in.read_text()))
-        # There should only be one Cython source file
-        cython_source, = tmp_path.glob('*.pyx')
-        pip_install = pip + ['install', '--verbose']
-        if editable:
-            pip_install += ['--editable', tmpdir]
-        else:
-            pip_install.append(tmpdir)
-        try:
-            subprocess.run(pip_install).check_returncode()
-            subprocess.run(pip + ['list']).check_returncode()
-            yield cython_source, module
-        finally:
-            pip_uninstall = pip + ['uninstall', '--verbose', '--yes', module]
-            subprocess.run(pip_uninstall).check_returncode()
+    tmp_path = tmp_path_factory.mktemp('cython_example')
+    # Replace all references to `cython_example` with the actual module
+    # name and write to the tempdir
+    for prefix, _, files in os.walk(source):
+        dir_in = Path(prefix)
+        for fname in files:
+            _, ext = os.path.splitext(fname)
+            if ext not in {'.py', '.pyx', '.pxd', '.toml'}:
+                continue
+            dir_out = tmp_path.joinpath(*(
+                replace(part) for part in dir_in.relative_to(source).parts))
+            dir_out.mkdir(exist_ok=True)
+            file_in = dir_in / fname
+            file_out = dir_out / replace(fname)
+            file_out.write_text(replace(file_in.read_text()))
+    # There should only be one Cython source file
+    cython_source, = tmp_path.glob('*.pyx')
+    pip_install = pip + ['install', '--verbose']
+    if editable:
+        pip_install += ['--editable', str(tmp_path)]
+    else:
+        pip_install.append(str(tmp_path))
+    try:
+        subprocess.run(pip_install).check_returncode()
+        subprocess.run(pip + ['list']).check_returncode()
+        yield cython_source, module
+    finally:
+        pip_uninstall = pip + ['uninstall', '--verbose', '--yes', module]
+        subprocess.run(pip_uninstall).check_returncode()
 
 
 @pytest.fixture(scope='module')
-def cython_example() -> Tuple[Path, ModuleType]:
+def cython_example(
+        tmp_path_factory: pytest.TempPathFactory) -> Tuple[Path, ModuleType]:
     """
     Install the example Cython module, yield the path to the Cython
     source file and the corresponding module, uninstall it at teardown.
     """
     # With editable installs, we need to refresh `sys.meta_path` before
     # the installed module is available
-    for path, mod_name in _install_cython_example(True):
+    for path, mod_name in _install_cython_example(tmp_path_factory, True):
         reload(import_module('site'))
         yield (path, import_module(mod_name))
 
