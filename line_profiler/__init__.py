bc6e9c1b9c363a1e6101daaa4a2f13492ebf5f4c
Jon Crall <erotemic@gmail.com>
2025-07-22 14:29:24 -0400

    Dev/prep 5x release (#363)
    * Bump version to 5.0.0
    
    * rotate secrets

--- line_profiler/__init__.py
+++ line_profiler/__init__.py
@@ -249,7 +249,7 @@ mkinit ./line_profiler/__init__.py --relative -w
 # from .line_profiler import __version__
 
 # NOTE: This needs to be in sync with ../kernprof.py and line_profiler.py
-__version__ = '4.3.0'
+__version__ = '5.0.0'
 
 from .line_profiler import (LineProfiler,
                             load_ipython_extension, load_stats, main,
