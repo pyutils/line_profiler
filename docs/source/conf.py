0b32904897bff5d91886cf2476e3bb98638cb31e
joncrall <erotemic@gmail.com>
2025-07-29 18:35:33 -0400

    Update xcookie

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,14 +16,15 @@ Notes:
 
     # need to edit the conf.py
 
+    # Remove any old auto docs folder and regenerate it.
+    rm -rf ~/code/line_profiler/docs/source/auto
     cd ~/code/line_profiler/docs
     sphinx-apidoc --private --separate --force --output-dir ~/code/line_profiler/docs/source/auto ~/code/line_profiler/line_profiler
+    git add source/auto/*.rst
 
     # Note: the module should importable before running this
     # (e.g. install it in developer mode or munge the PYTHONPATH)
     make html
-
-    git add source/auto/*.rst
 
     Also:
         To turn on PR checks
@@ -83,6 +84,7 @@ Notes:
                 push events,
                 tag push events,
                 merge request events
+                release events
 
             Click the "Add webhook" button.
 
