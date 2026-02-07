88590a06277ad68346edd78cfa96ec2d615efec7
joncrall <erotemic@gmail.com>
2026-02-07 13:32:44 -0500

    Fix version consistency

--- line_profiler/line_profiler.py
+++ line_profiler/line_profiler.py
@@ -34,7 +34,7 @@ from .toml_config import ConfigSource
 
 
 # NOTE: This needs to be in sync with ../kernprof.py and __init__.py
-__version__ = '5.0.1'
+__version__ = '5.0.2'
 
 
 @functools.lru_cache()
