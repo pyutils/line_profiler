2b28dd7cb6be3143852a69678498a44f766fc969
Test User <test@example.com>
2026-02-08 15:16:15 -0500

    Fix type error in 38

--- line_profiler/autoprofile/autoprofile.py
+++ line_profiler/autoprofile/autoprofile.py
@@ -52,7 +52,7 @@ import operator
 import sys
 import types
 from collections.abc import MutableMapping
-from typing import Any, cast
+from typing import Any, cast, Dict
 from .ast_tree_profiler import AstTreeProfiler
 from .run_module import AstTreeModuleProfiler
 from .line_profiler_utils import add_imported_function_or_module
@@ -150,4 +150,4 @@ def run(script_file: str, ns: MutableMapping[str, Any],
     code_obj = compile(tree_profiled, script_file, 'exec')
     with ctx as callback:
         callback()
-        exec(code_obj, cast(dict[str, Any], namespace), namespace)
+        exec(code_obj, cast(Dict[str, Any], namespace), namespace)
