78c153373b9f175c6b68aae05bae3ac626e0a849
Terence S.-C. Tsang <t.tsang.sci@gmail.com>
2026-04-09 03:56:57 +0200

    WIP: refactoring `tests/test_child_procs.py`
    tests/test_child_procs.py
        test_running_multiproc_literal_code()
            New test paralleling `test_running_multiproc_{script,module}` to
            test `kernprof -c ...`
        test_multiproc_script_sanity_check()
            - Refactored parameters for better `pytest` output
            - Added testing for running the code with `python -c ...`
        <Misc>
            - Added CLI argument `--local` to the profiled module to toggle
              between a locally-defined summing function and an imported one
            - Refactored how the test modules are injected
            - Added debugging output to `subprocess.run()` calls
            - Added provisional support for examining the profiling data

--- tests/test_child_procs.py
+++ tests/test_child_procs.py
@@ -1,33 +1,61 @@
 from __future__ import annotations
 
 import os
+import shlex
 import subprocess
 import sys
-from collections.abc import Callable, Generator, Mapping
+from collections.abc import (
+    Callable, Collection, Generator, Mapping, Sequence,
+)
+from dataclasses import dataclass
+from functools import partial
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from textwrap import dedent, indent
 
 import pytest
 import ubelt as ub
+from _pytest.fixtures import FixtureFunctionDefinition
+
+from line_profiler.line_profiler import LineStats
 
 
 NUM_NUMBERS = 100
 NUM_PROCS = 4
+
+EXTERNAL_MODULE_BODY = dedent("""
+from __future__ import annotations
+
+
+def my_external_sum(x: list[int]) -> int:
+    result: int = 0  # GREP_MARKER[EXT-INVOCATION]
+    for item in x:
+        result += item  # GREP_MARKER[EXT-LOOP]
+    return result
+""").strip('\n')
+
 TEST_MODULE_BODY = dedent(f"""
 from __future__ import annotations
+
 from argparse import ArgumentParser
+from collections.abc import Callable
 from multiprocessing import Pool
 
+from my_ext_module import my_external_sum
 
-def my_sum(x: list[int]) -> int:
-    result: int = 0
-    for item in x:
-        result += item
+
+def my_local_sum(x: list[int]) -> int:
+    result: int = 0  # GREP_MARKER[LOCAL-INVOCATION]
+    # The reversing is to prevent bytecode aliasing with
+    # `my_external_sum()` (see issue #424, PR #425)
+    for item in reversed(x):
+        result += item  # GREP_MARKER[LOCAL-LOOP]
     return result
 
 
-def sum_in_child_procs(length: int, n: int) -> int:
+def sum_in_child_procs(
+    length: int, n: int, my_sum: Callable[[list[int]], int],
+) -> int:
     my_list: list[int] = list(range(1, length + 1))
     sublists: list[list[int]] = []
     subsums: list[int]
@@ -48,8 +76,15 @@ def main(args: list[str] | None = None) -> None:
     parser = ArgumentParser()
     parser.add_argument('-l', '--length', type=int, default={NUM_NUMBERS})
     parser.add_argument('-n', type=int, default={NUM_PROCS})
+    parser.add_argument(
+        '--local',
+        action='store_const',
+        dest='my_sum',
+        default=my_external_sum,
+        const=my_local_sum,
+    )
     options = parser.parse_args(args)
-    print(sum_in_child_procs(options.length, options.n))
+    print(sum_in_child_procs(options.length, options.n, options.my_sum))
 
 
 if __name__ == '__main__':
@@ -57,37 +92,79 @@ if __name__ == '__main__':
 """).strip('\n')
 
 
-@pytest.fixture(scope='session')
-def test_module() -> Generator[Path, None, None]:
-    with TemporaryDirectory() as mydir_str:
-        my_dir = Path(mydir_str)
-        my_dir.mkdir(exist_ok=True)
-        my_module = my_dir / 'my_test_module.py'
-        with my_module.open('w') as fobj:
-            fobj.write(TEST_MODULE_BODY + '\n')
-        yield my_module
+@dataclass
+class _ModuleFixture:
+    path: Path
+    monkeypatch: pytest.MonkeyPatch
+    dependencies: Collection[_ModuleFixture] = ()
+
+    def install(
+        self, *,
+        local: bool = False, children: bool = False, deps_only: bool = False,
+    ) -> None:
+        for dep in self.dependencies:
+            dep.install(local=local, children=children)
+        if deps_only:
+            return
+        path = str(self.path.parent)
+        if local:
+            self.monkeypatch.syspath_prepend(path)
+        if children:
+            self.monkeypatch.setenv('PYTHONPATH', path, prepend=os.pathsep)
+
+    @property
+    def name(self) -> str:
+        return self.path.stem
 
 
-@pytest.mark.parametrize('as_module', [True, False])
-@pytest.mark.parametrize(
-    ('nnums', 'nprocs'), [(None, None), (None, 3), (200, None)],
+def _module_path_fixture(
+    name: str, body: str, **kwargs,
+) -> FixtureFunctionDefinition:
+    def my_fixture_func() -> Generator[Path, None, None]:
+        with TemporaryDirectory() as mydir_str:
+            my_dir = Path(mydir_str)
+            my_dir.mkdir(exist_ok=True)
+            my_module = my_dir / f'{name}.py'
+            my_module.write_text(body)
+            yield my_module
+
+    my_fixture_func.__name__ = name
+    return pytest.fixture(my_fixture_func, **kwargs)
+
+
+# Only write the files once per test session
+_ext_module = _module_path_fixture(
+    'my_ext_module', EXTERNAL_MODULE_BODY, scope='session',
 )
-def test_multiproc_script_sanity_check(
-    test_module: Path,
-    tmp_path_factory: pytest.TempPathFactory,
-    nnums: int,
-    nprocs: int,
-    as_module: bool,
-) -> None:
+_test_module = _module_path_fixture(
+    'my_test_module', TEST_MODULE_BODY, scope='session',
+)
+
+
+@pytest.fixture
+def ext_module(
+    _ext_module: Path, monkeypatch: pytest.MonkeyPatch,
+) -> Generator[_ModuleFixture, None, None]:
     """
-    Sanity check that the test module functions as expected when run
-    with vanilla Python.
+    Yields:
+        :py:class:`_ModuleFixture` helper object containing the code at
+        :py:data:`EXTERNAL_MODULE_BODY`
     """
-    _run_test_module(
-        _run_as_module if as_module else _run_as_script,
-        test_module, tmp_path_factory, [sys.executable], None, False,
-        nnums=nnums, nprocs=nprocs,
-    )
+    yield _ModuleFixture(_ext_module, monkeypatch)
+
+
+@pytest.fixture
+def test_module(
+    _test_module: Path,
+    ext_module: _ModuleFixture,
+    monkeypatch: pytest.MonkeyPatch,
+) -> Generator[_ModuleFixture, None, None]:
+    """
+    Yields:
+        :py:class:`_ModuleFixture` helper object containing the code at
+        :py:data:`TEST_MODULE_BODY`
+    """
+    yield _ModuleFixture(_test_module, monkeypatch, [ext_module])
 
 
 # Note:
@@ -99,20 +176,20 @@ def test_multiproc_script_sanity_check(
 fuzz_invocations = pytest.mark.parametrize(
     ('runner', 'outfile', 'profile',
      'label'),  # Dummy argument to make `pytest` output more legible
-    [
-        (['kernprof', '-q'], 'out.prof', False, 'cProfile'),
-        # Run with `line_profiler` with and w/o profiling targets
-        (['kernprof', '-q', '-l'], 'out.lprof', False,
-         'line_profiler-inactive'),
-        (['kernprof', '-q', '-l'], 'out.lprof', True,
-         'line_profiler-active'),
-    ],
+    # This is essentially a no-op since it doesn't actually do
+    # line-profiling, but we check that code path for completeness
+    [(['kernprof', '-q', '--no-line'], 'out.prof', False, 'cProfile')]
+    # Run line profiling with and w/o profiling targets
+    + [(['kernprof', '-q', '-l'], 'out.lprof', False,
+        'line_profiler-inactive'),
+       (['kernprof', '-q', '-l'], 'out.lprof', True,
+        'line_profiler-active')],
 )
 
 
 @fuzz_invocations
 def test_running_multiproc_script(
-    test_module: Path,
+    test_module: _ModuleFixture,
     tmp_path_factory: pytest.TempPathFactory,
     runner: str | list[str],
     outfile: str | None,
@@ -123,15 +200,12 @@ def test_running_multiproc_script(
     Check that `kernprof` can run the test module as a script
     (`kernprof [...] <path>`).
     """
-    _run_test_module(
-        _run_as_script,
-        test_module, tmp_path_factory, runner, outfile, profile,
-    )
+    run_script(test_module, tmp_path_factory, runner, outfile, profile)
 
 
 @fuzz_invocations
 def test_running_multiproc_module(
-    test_module: Path,
+    test_module: _ModuleFixture,
     tmp_path_factory: pytest.TempPathFactory,
     runner: str | list[str],
     outfile: str | None,
@@ -142,62 +216,150 @@ def test_running_multiproc_module(
     Check that `kernprof` can run the test module as a module
     (`kernprof [...] -m <module>`).
     """
-    _run_test_module(
-        _run_as_module,
-        test_module, tmp_path_factory, runner, outfile, profile,
-    )
+    run_module(test_module, tmp_path_factory, runner, outfile, profile)
 
 
-def _run_as_script(
-    runner_args: list[str], test_args: list[str], test_module: Path, **kwargs
-) -> subprocess.CompletedProcess:
-    cmd = runner_args + [str(test_module)] + test_args
-    return subprocess.run(cmd, **kwargs)
-
-
-def _run_as_module(
-    runner_args: list[str],
-    test_args: list[str],
-    test_module: Path,
-    *,
-    env: Mapping[str, str] | None = None,
-    **kwargs
-) -> subprocess.CompletedProcess:
-    cmd = runner_args + ['-m', test_module.stem] + test_args
-    env_dict = {**os.environ, **(env or {})}
-    python_path = env_dict.pop('PYTHONPATH', '')
-    if python_path:
-        env_dict['PYTHONPATH'] = '{}:{}'.format(
-            test_module.parent, python_path,
-        )
-    else:
-        env_dict['PYTHONPATH'] = str(test_module.parent)
-    return subprocess.run(cmd, env=env_dict, **kwargs)
-
-
-def _run_test_module(
-    run_helper: Callable[..., subprocess.CompletedProcess],
-    test_module: Path,
+@fuzz_invocations
+def test_running_multiproc_literal_code(
+    test_module: _ModuleFixture,
     tmp_path_factory: pytest.TempPathFactory,
     runner: str | list[str],
     outfile: str | None,
     profile: bool,
+    label: str,
+) -> None:
+    """
+    Check that `kernprof` can run the test module as literal code
+    (`kernprof [...] -c "code"`).
+    """
+    run_literal_code(test_module, tmp_path_factory, runner, outfile, profile)
+
+
+def _run_as_script(
+    runner_args: list[str], test_args: list[str], test_module: _ModuleFixture,
+    **kwargs
+) -> subprocess.CompletedProcess:
+    cmd = runner_args + [str(test_module.path)] + test_args
+    test_module.install(children=True, deps_only=True)
+    return _run_subproc(cmd, **kwargs)
+
+
+def _run_as_module(
+    runner_args: list[str], test_args: list[str], test_module: _ModuleFixture,
+    **kwargs
+) -> subprocess.CompletedProcess:
+    cmd = runner_args + ['-m', test_module.name] + test_args
+    test_module.install(children=True)
+    return _run_subproc(cmd, **kwargs)
+
+
+def _run_as_literal_code(
+    runner_args: list[str], test_args: list[str], test_module: _ModuleFixture,
+    **kwargs
+) -> subprocess.CompletedProcess:
+    cmd = runner_args + ['-c', test_module.path.read_text()] + test_args
+    test_module.install(children=True, deps_only=True)
+    return _run_subproc(cmd, **kwargs)
+
+
+def _run_subproc(
+    cmd: Sequence[str] | str,
+    /,
+    *args,
+    env: Mapping[str, str] | None = None,
+    **kwargs
+) -> subprocess.CompletedProcess:
+    """
+    Wrapper around :py:func:`subprocess.run` which writes debugging
+    output.
+    """
+    if isinstance(cmd, str):
+        cmd_str = cmd
+    else:
+        cmd_str = shlex.join(cmd)
+    print('Command:', cmd_str)
+    if env is not None:
+        diff: list[str] = []
+        for key in set(os.environ).union(env):
+            old = os.environ.get(key)
+            new = env.get(key)
+            if old is not None is new:
+                item = f'{old!r} -> (deleted)'
+            elif old is None is not new:
+                item = f'{new!r} (added)'
+            else:
+                if old == new:
+                    continue
+                item = f'{old!r} -> {new!r}'
+            diff.append(f'${{{key}}}: {item}')
+        if diff:
+            print('Env:', indent('\n'.join(diff), '  '), sep='\n')
+    print('-- Process start --')
+    # Note: somehow `mypy` doesn't agree with simply unpacking the
+    # `*args` into `subprocess.run()`...
+    status: int | str = '???'
+    try:
+        proc = subprocess.run(  # type: ignore[call-overload]
+            cmd, *args, env=env, **kwargs,
+        )
+    except Exception:
+        status = 'error'
+        raise
+    else:
+        status = proc.returncode
+        return proc
+    finally:
+        print(f'-- Process end (return status: {status})--')
+
+
+def _run_test_module(
+    run_helper: Callable[..., subprocess.CompletedProcess],
+    test_module: _ModuleFixture,
+    tmp_path_factory: pytest.TempPathFactory,
+    runner: str | list[str] = 'kernprof',
+    outfile: str | None = None,
+    profile: bool = True,
     *,
+    profiled_code_is_tempfile: bool = False,
     nnums: int | None = None,
     nprocs: int | None = None,
     check: bool = True,
-) -> tuple[subprocess.CompletedProcess, Path | None]:
+    nhits: Mapping[str, int] | None = None,
+) -> tuple[subprocess.CompletedProcess, LineStats | None]:
     """
-    Return
-    ------
-    `(process_running_the_test_module, path_to_profiling_output | None)`
+    Returns:
+        process_running_the_test_module (subprocess.CompletedProcess):
+            Process object
+        profliing_stats (LineStats | None):
+            Line-profiling stats (where available)
     """
+    def check_output(output: str, tag: str, nhits: int) -> None:
+        # The line should be preixed with 5 numbers:
+        # lineno, nhits, time, time-per-hit, % time
+        actual_nhits = 0
+        for line in output.splitlines():
+            if line.endswith(f'# GREP_MARKER[{tag}]'):
+                try:
+                    _, n, _, _, _, *_ = line.split()
+                    actual_nhits += int(n)
+                except Exception:
+                    pass
+        assert actual_nhits == nhits
+
     if isinstance(runner, str):
         runner_args: list[str] = [runner]
     else:
         runner_args = list(runner)
-    if profile:
-        runner_args.extend(['--prof-mod', str(test_module)])
+
+    if not profile:
+        nhits = None
+
+    if profile and not profiled_code_is_tempfile:
+        runner_args.extend(['--prof-mod', str(test_module.path)])
+    if nhits is not None:
+        # We need `kernprof` to write the profliing results immediately
+        # to preserve data from tempfiles (see note below)
+        runner_args.append('--view')
 
     test_args: list[str] = []
     if nnums is None:
@@ -221,13 +383,60 @@ def _run_test_module(
             print(f'stdout:\n{indent(proc.stdout, "  ")}')
             print(f'stderr:\n{indent(proc.stderr, "  ")}', file=sys.stderr)
 
-        assert proc.stdout == f'{nnums * (nnums + 1) // 2}\n'
-
-        prof_result: Path | None = None
+        # Checks:
+        # - The result is correctly calculated
+        assert proc.stdout.splitlines()[0] == str(nnums * (nnums + 1) // 2)
+        # - Profiling results are written to the specified file
+        prof_result: LineStats | None = None
         if outfile is None:
             assert not list(Path.cwd().iterdir())
         else:
-            prof_result = Path(outfile).resolve()
-            assert prof_result.exists()
-            assert prof_result.stat().st_size
+            assert os.path.exists(outfile)
+            assert os.stat(outfile).st_size
+            if profile:
+                prof_result = LineStats.from_files(outfile)
+        # - If we're keeping track, the function is called the expected
+        #   number of times and has run the expected # of loops
+        #   (Note: we do it by parsing the output of `kernprof -v`
+        #   instead of reading the `--outfile`, because if the profiled
+        #   code is in a tempfile the profiling data will be dropped in
+        #   the written outfile)
+        for tag, num in (nhits or {}).items():
+            check_output(proc.stdout, tag, num)
     return proc, prof_result
+
+
+run_module = partial(_run_test_module, _run_as_module)
+run_script = partial(_run_test_module, _run_as_script)
+run_literal_code = partial(
+    _run_test_module, _run_as_literal_code, profiled_code_is_tempfile=True,
+)
+
+
+@pytest.mark.parametrize(
+    ('run_func',
+     'label'),  # Dummy argument to make `pytest` output more legible
+    [(run_module, 'module'),
+     (run_script, 'script'),
+     (run_literal_code, 'literal-code')],
+)
+@pytest.mark.parametrize(
+    ('nnums', 'nprocs'), [(None, None), (None, 3), (200, None)],
+)
+def test_multiproc_script_sanity_check(
+    run_func: Callable[..., subprocess.CompletedProcess],
+    test_module: _ModuleFixture,
+    tmp_path_factory: pytest.TempPathFactory,
+    nnums: int,
+    nprocs: int,
+    label: str,
+) -> None:
+    """
+    Sanity check that the test module functions as expected when run
+    with vanilla Python.
+    """
+    run_func(
+        test_module, tmp_path_factory,
+        runner=sys.executable, profile=False,
+        nnums=nnums, nprocs=nprocs,
+    )
