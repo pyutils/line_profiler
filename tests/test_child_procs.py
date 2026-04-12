ddb211c0fd265a7c0521c4c6c1ec5b92d4ffb50c
Terence S.-C. Tsang <t.tsang.sci@gmail.com>
2026-04-12 05:42:26 +0200

    WIP: flesh out the child-proc tests more
    tests/test_child_procs.py
        TEST_MODULE_BODY, [_]test_module()
            Added CLI flag to select `multiprocessing` start methods
        _Params
            New convenience class for test parametrization
        test_multiproc_script_sanity_check()
            - Streamlined parametrization (15 subtests -> 10)
            - Added subtests for various `multiprocessing` start methods
        test_multiproc_script_sanity_check()
            - Streamlined parametrization (24 subtests -> 21)
            - Added subtests for various `multiprocessing` start methods

--- tests/test_child_procs.py
+++ tests/test_child_procs.py
@@ -1,18 +1,22 @@
 from __future__ import annotations
 
+import enum
 import os
 import shlex
 import subprocess
 import sys
 from collections.abc import (
-    Callable, Collection, Generator, Mapping, Sequence,
+    Callable, Collection, Generator, Iterable, Mapping, Sequence,
 )
 from dataclasses import dataclass
 from functools import partial
+from multiprocessing import get_all_start_methods
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from textwrap import dedent, indent
 from time import monotonic
+from typing import Any, Literal, TypeVar, cast, final, overload
+from typing_extensions import Self
 from uuid import uuid4
 
 import pytest
@@ -21,8 +25,12 @@ import ubelt as ub
 from line_profiler.line_profiler import LineStats
 
 
+T = TypeVar('T')
+C = TypeVar('C', bound=Callable[..., Any])
+
 NUM_NUMBERS = 100
 NUM_PROCS = 4
+START_METHODS = set(get_all_start_methods())
 
 EXTERNAL_MODULE_BODY = dedent("""
 from __future__ import annotations
@@ -40,7 +48,8 @@ from __future__ import annotations
 
 from argparse import ArgumentParser
 from collections.abc import Callable
-from multiprocessing import Pool
+from multiprocessing import get_context, Pool
+from typing import Literal
 
 from {EXT_MODULE} import my_external_sum
 
@@ -56,6 +65,7 @@ def my_local_sum(x: list[int]) -> int:
 
 def sum_in_child_procs(
     length: int, n: int, my_sum: Callable[[list[int]], int],
+    start_method: Literal['fork', 'forkserver', 'spawn'] | None = None,
 ) -> int:
     my_list: list[int] = list(range(1, length + 1))
     sublists: list[list[int]] = []
@@ -66,7 +76,11 @@ def sum_in_child_procs(
     while my_list:
         sublist, my_list = my_list[:sublength], my_list[sublength:]
         sublists.append(sublist)
-    with Pool(n) as pool:
+    if start_method:
+        pool = get_context(start_method).Pool(n)
+    else:
+        pool = Pool(n)
+    with pool:
         subsums = pool.map(my_sum, sublists)
         pool.close()
         pool.join()
@@ -77,6 +91,10 @@ def main(args: list[str] | None = None) -> None:
     parser = ArgumentParser()
     parser.add_argument('-l', '--length', type=int, default={NUM_NUMBERS})
     parser.add_argument('-n', type=int, default={NUM_PROCS})
+    parser.add_argument(
+        '-s', '--start-method',
+        choices=['fork', 'forkserver', 'spawn'], default=None,
+    )
     parser.add_argument(
         '--local',
         action='store_const',
@@ -206,6 +224,197 @@ def test_module(
     yield _ModuleFixture(_test_module, monkeypatch, [ext_module])
 
 
+# ========================== Helper functions ==========================
+
+
+class _NotSupplied(enum.Enum):
+    NOT_SUPPLIED = enum.auto()
+
+
+@final
+@dataclass
+class _Params:
+    """
+    Convenience wrapper around :py:func:`pytest.mark.parametrize`.
+    """
+    params: tuple[str, ...]
+    values: list[tuple[Any, ...]]
+    defaults: tuple[Any, ...]
+
+    def __post_init__(self) -> None:
+        n = len(self.params)
+        assert all(p.isidentifier() for p in self.params)  # Validity
+        assert len(set(self.params)) == n  # Uniqueness
+        assert len(self.defaults) == n  # Consistency
+        self.values = list(self._unique(self.values))
+        assert all(len(v) == n for v in self.values)
+
+    def __mul__(self, other: Self) -> Self:
+        """
+        Form a Cartesian product between the two instances with disjoint
+        :py:attr:`~.params`, like stacking the
+        :py:func:`pytest.mark.parametrize `decorators.
+
+        Example:
+            >>> p1 = _Params.new(('a', 'b'), [(0, 0), (1, 2), (3, 4)],
+            ...                  defaults=(1, 2))
+            >>> p2 = _Params.new('c', [0, 5, 6])
+            >>> p1 * p2  # doctest: +NORMALIZE_WHITESPACE
+            _Params(params=('a', 'b', 'c'),
+                    values=[(0, 0, 0), (0, 0, 5), (0, 0, 6),
+                            (1, 2, 0), (1, 2, 5), (1, 2, 6),
+                            (3, 4, 0), (3, 4, 5), (3, 4, 6)],
+                    defaults=(1, 2, 0))
+        """
+        assert not set(self.params) & set(other.params)
+        return type(self)(
+            self.params + other.params,
+            [sv + ov for sv in self.values for ov in other.values],
+            self.defaults + other.defaults,
+        )
+
+    def __add__(self, other: Self) -> Self:
+        """
+        Concatenate two instances:
+
+        - For parameters appearing in both, their lists of values are
+          concatenated.
+
+        - For parameters appearing in either instance, the missing
+          values are taken from the other instance's
+          :py:attr:`~.defaults`.
+
+        Note:
+            In the case of clashes, the :py:attr:`~.defaults` and the
+            order of the :py:attr:`~.params` of ``self`` (the left
+            operand) take precedence.
+
+        Example:
+            >>> p1 = _Params.new(('a', 'b', 'c'),
+            ...                  [(0, 0, 0),  # defaults
+            ...                   (1, 2, 3), (4, 5, 6)])
+            >>> p2 = _Params.new(('c', 'd'), [(7, 8), (9, 10)],
+            ...                  defaults=(-1, -1))
+            >>> p1 + p2  # doctest: +NORMALIZE_WHITESPACE
+            _Params(params=('a', 'b', 'c', 'd'),
+                    values=[(0, 0, 0, -1),
+                            (1, 2, 3, -1),
+                            (4, 5, 6, -1),
+                            (0, 0, 7, 8),
+                            (0, 0, 9, 10)],
+                    defaults=(0, 0, 0, -1))
+        """
+        self_defaults = dict(zip(self.params, self.defaults))
+        other_defaults = dict(zip(other.params, other.defaults))
+        new_params = tuple(self._unique(self.params + other.params))
+
+        defaults = {**other_defaults, **self_defaults}
+        new_defaults_tuple = tuple(defaults[p] for p in new_params)
+
+        new_values: list[tuple[Any, ...]] = []
+        for old_values, old_params in [
+            (self.values, self.params), (other.values, other.params),
+        ]:
+            indices: list[
+                tuple[Literal[True], int] | tuple[Literal[False], str]
+            ] = [
+                (True, old_params.index(p)) if p in old_params else (False, p)
+                for p in new_params
+            ]
+            new_values.extend(
+                tuple(
+                    (
+                        value[cast(int, index)]
+                        if available else
+                        defaults[cast(str, index)]
+                    ) for available, index in indices
+                )
+                for value in old_values
+            )
+        return type(self)(new_params, new_values, new_defaults_tuple)
+
+    def __call__(self, func: C) -> C:
+        """
+        Mark a callable as with :py:func:`pytest.mark.parametrize`.
+        """
+        # Note: `pytest` automatically assumes single-param values to
+        # be unpackes, so comply here
+        if len(self.params) == 1:
+            marker = pytest.mark.parametrize(
+                self.params[0], [v[0] for v in self.values],
+            )
+        else:
+            marker = pytest.mark.parametrize(self.params, self.values)
+        return marker(func)
+
+    @staticmethod
+    def _unique(items: Iterable[T]) -> Generator[T, None, None]:
+        seen: set[T] = set()
+        for item in items:
+            if item in seen:
+                continue
+            seen.add(item)
+            yield item
+
+    @overload
+    @classmethod
+    def new(
+        cls,
+        params: Sequence[str] | str,
+        values: Sequence[Sequence[Any]],
+        defaults: Sequence[Any] | _NotSupplied = _NotSupplied.NOT_SUPPLIED,
+    ) -> Self:
+        ...
+
+    @overload
+    @classmethod
+    def new(
+        cls,
+        params: str,
+        values: Sequence[Any],
+        defaults: Any | _NotSupplied = _NotSupplied.NOT_SUPPLIED,
+    ) -> Self:
+        ...
+
+    @classmethod
+    def new(
+        cls,
+        params: Sequence[str] | str,
+        values: Sequence[Sequence[Any]] | Sequence[Any],
+        defaults: (
+            Sequence[Any] | Any | _NotSupplied
+        ) = _NotSupplied.NOT_SUPPLIED,
+    ) -> Self:
+        """
+        Instantiator more akin to :py:func:`pytest.mark.parametrize`:
+
+        - ``params`` can be provided as a comma-separated string
+
+        - Single parameters can be unpacked (singular param-name string
+          and param-value sequences)
+
+        - If ``defaults`` are not given, it is implicitly set to the
+          FIRST item in ``values``.
+        """
+        if isinstance(params, str):
+            param_list: tuple[str, ...] = tuple(
+                p.strip() for p in params.split(',')
+            )
+            unpacked = len(param_list) == 1
+        else:
+            param_list = tuple(params)
+            unpacked = False
+        if defaults == _NotSupplied.NOT_SUPPLIED:
+            defaults, *_ = values
+        if unpacked:
+            default_values: tuple[Any, ...] = defaults,
+            value_tuple_list: list[tuple[Any, ...]] = [(v,) for v in values]
+        else:
+            default_values = tuple(defaults)  # type: ignore[arg-type]
+            value_tuple_list = [tuple(v) for v in values]
+        return cls(param_list, value_tuple_list, default_values)
+
+
 def _run_as_script(
     runner_args: list[str], test_args: list[str], test_module: _ModuleFixture,
     **kwargs
@@ -307,6 +516,7 @@ def _run_test_module(
     *,
     profiled_code_is_tempfile: bool = False,
     use_local_func: bool = False,
+    start_method: Literal['fork', 'forkserver', 'spawn'] | None = None,
     nnums: int | None = None,
     nprocs: int | None = None,
     check: bool = True,
@@ -350,6 +560,14 @@ def _run_test_module(
     test_args: list[str] = []
     if use_local_func:
         test_args.append('--local')
+    if start_method:
+        if start_method in START_METHODS:
+            test_args.extend(['-s', start_method])
+        else:
+            pytest.skip(
+                f'`multiprocessing` start method {start_method!r} '
+                'not available on the platform'
+            )
     if nnums is None:
         nnums = NUM_NUMBERS
     else:
@@ -393,27 +611,38 @@ run_literal_code = partial(
     _run_test_module, _run_as_literal_code, profiled_code_is_tempfile=True,
 )
 
+# =============================== Tests ================================
 
-@pytest.mark.parametrize(
-    ('run_func', 'use_local_func',
-     'label'),  # Dummy argument to make `pytest` output more legible
-    [(run_module, True, 'module-local'), (run_module, False, 'module-ext'),
-     (run_script, True, 'script-local'), (run_script, False, 'script-ext')]
+
+_fuzz_sanity = (
+    _Params.new(('run_func', 'label1'),
+                [(run_module, 'module'), (run_script, 'script')])
+    * _Params.new(('use_local_func', 'label2'),
+                  [(True, 'local'), (False, 'ext')])
     # Python can't pickle things unless they resided in a retrievable
     # location (so not the script supplied by `python -c`)
-    + [(run_literal_code, False, 'literal-code-ext')],
+    + _Params.new(('run_func', 'label1', 'use_local_func', 'label2'),
+                  [(run_literal_code, 'literal-code', False, 'ext')])
+    # Also fuzz the parallelization-related stuff
+    + _Params.new('start_method', ['fork', 'forkserver', 'spawn'],
+                  defaults=None)
+    + _Params.new(('nnums', 'nprocs'), [(200, None), (None, 3)],
+                  defaults=(None, None))
 )
-@pytest.mark.parametrize(
-    ('nnums', 'nprocs'), [(None, None), (None, 3), (200, None)],
-)
+
+
+@_fuzz_sanity
 def test_multiproc_script_sanity_check(
     run_func: Callable[..., subprocess.CompletedProcess],
     test_module: _ModuleFixture,
     tmp_path_factory: pytest.TempPathFactory,
     use_local_func: bool,
-    nnums: int,
-    nprocs: int,
-    label: str,
+    start_method: Literal['fork', 'forkserver', 'spawn'] | None,
+    nnums: int | None,
+    nprocs: int | None,
+    # Dummy arguments to make `pytest` output more legible
+    label1: str,
+    label2: str,
 ) -> None:
     """
     Sanity check that the test module functions as expected when run
@@ -423,20 +652,20 @@ def test_multiproc_script_sanity_check(
         test_module, tmp_path_factory,
         runner=sys.executable, profile=False,
         use_local_func=use_local_func,
+        start_method=start_method,
         nnums=nnums, nprocs=nprocs,
     )
 
 
 @pytest.mark.parametrize(
-    ('run_func',
-     'label2'),  # Dummy argument to make `pytest` output more legible
+    ('run_func', 'label1'),
     [(run_module, 'module'),
      (run_script, 'script'),
      (run_literal_code, 'literal-code')]
 )
 @pytest.mark.parametrize(
     ('runner', 'outfile', 'profile',
-     'label1'),  # Dummy argument to make `pytest` output more legible
+     'label2'),  # Dummy argument to make `pytest` output more legible
     # This is essentially a no-op since it doesn't actually do
     # line-profiling, but we check that code path for completeness
     [(['kernprof', '-q', '--no-line'], 'out.prof', False, 'cProfile')]
@@ -453,6 +682,7 @@ def test_running_multiproc_script(
     runner: str | list[str],
     outfile: str | None,
     profile: bool,
+    # Dummy arguments to make `pytest` output more legible
     label1: str,
     label2: str,
 ) -> None:
@@ -471,23 +701,29 @@ def test_running_multiproc_script(
     run_func(test_module, tmp_path_factory, runner, outfile, profile)
 
 
-@pytest.mark.parametrize(
-    ('run_func', 'label1'),
-    [(run_module, 'module'),
-     (run_script, 'script'),
-     (run_literal_code, 'literal-code')]
+_fuzz_prof_mp_1 = (
+    _Params.new(('run_func', 'label1'),
+                [(run_module, 'module'),
+                 (run_script, 'script'),
+                 (run_literal_code, 'literal-code')],
+                defaults=(run_script, 'script'))
+    + _Params.new(('prof_child_procs', 'label2'),
+                  [(True, 'with-child-prof'), (False, 'no-child-prof')])
+    + _Params.new('start_method', ['fork', 'forkserver', 'spawn'],
+                  defaults=None)
 )
-@pytest.mark.parametrize(
-    ('prof_child_procs', 'label2'),
-    [(True, 'with-child-prof'), (False, 'no-child-prof')],
+_fuzz_prof_mp_2 = (
+    _Params.new(('preimports', 'label3'),
+                [(True, 'with-preimports'), (False, 'no-preimports')],
+                defaults=(False, 'no-preimports'))
+    + _Params.new(('use_local_func', 'label4'),
+                  [(True, 'local'), (False, 'external')],
+                  defaults=(False, 'external'))
 )
-@pytest.mark.parametrize(
-    ('preimports', 'label3'),
-    [(True, 'with-preimports'), (False, 'no-preimports')],
-)
-@pytest.mark.parametrize(
-    ('use_local_func', 'label4'), [(True, 'local'), (False, 'external')],
-)
+
+
+@_fuzz_prof_mp_1
+@_fuzz_prof_mp_2
 @pytest.mark.parametrize(
     # XXX: should we explicitly test the single-proc case? We already
     # have quite a lot of subtests tho...
@@ -501,6 +737,7 @@ def test_profiling_multiproc_script(
     prof_child_procs: bool,
     preimports: bool,
     use_local_func: bool,
+    start_method: Literal['fork', 'forkserver', 'spawn'] | None,
     nnums: int,
     nprocs: int,
     # Dummy arguments to make `pytest` output more legible
@@ -573,7 +810,14 @@ def test_profiling_multiproc_script(
         outfile='out.lprof',
         profile=True,
         use_local_func=use_local_func,
+        start_method=start_method,
         nhits=nhits,
         nnums=nnums,
         nprocs=nprocs,
     )
+
+
+# TODO: test for profiling under the following circumstances:
+# - `os.system()`
+# - `subprocess.run()`
+# - Nested subprocesses
